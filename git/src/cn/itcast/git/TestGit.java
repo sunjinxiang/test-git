112f834c960625ef6bb40b8958a617d9b37fba8a
张三 <zhangsan@itcast.cn>
2018-10-29 21:30:07 +0800

    再次修改

--- git/src/cn/itcast/git/TestGit.java
+++ git/src/cn/itcast/git/TestGit.java
@@ -3,9 +3,11 @@ package cn.itcast.git;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("222");
     }
 
     public void test(){
         System.out.println("111");
+        System.out.println("222");
     }
 }
